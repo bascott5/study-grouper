9437acbfd33a4eb6d603e2e4eb14ff8d39d01dda
Adam Cichoski <cjadam003@gmail.com>
2024-10-01 09:53:35 -0400

    Initial Commit

---
+++ src/main/java/com/csc340/studyGrouper/StudyGrouperApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.studyGrouper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGrouperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGrouperApplication.class, args);
+	}
+
+}
