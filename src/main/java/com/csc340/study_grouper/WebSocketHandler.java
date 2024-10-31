7f91c45cb73d9c4fd7f816d4d2c093e23cd22f80
Bennett <bascott1002@gmail.com>
2024-10-31 16:26:23 -0400

    Added websocket configuration

---
+++ src/main/java/com/csc340/study_grouper/WebSocketHandler.java
@@ -0,0 +1,33 @@
+// reference: https://medium.com/@ropelife/real-time-communication-with-websocket-in-spring-boot-using-webflux-5d9fbb36a0ab
+package com.csc340.study_grouper;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private static Set<WebSocketSession> sessions = new HashSet();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen()) {
+                try {
+                    webSocketSession.sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
