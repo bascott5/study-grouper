96751a33feeb83df4c4457d70e0b0a5834bf638d
Adam Cichoski <cjadam003@gmail.com>
2024-11-14 12:36:00 -0500

    Updated provider home view. StudyGroup finder now displays groups properly, but the search field needs some fixing. Does not store the query properly for some reason.

--- src/main/java/com/csc340/study_grouper/study_groups/StudyGroupAndInstructorRepository.java
+++ src/main/java/com/csc340/study_grouper/study_groups/StudyGroupAndInstructorRepository.java
@@ -9,9 +9,11 @@ import java.util.List;
 @Repository
 public interface StudyGroupAndInstructorRepository extends JpaRepository<StudyGroupAndInstructor, Integer> {
 
-  @Query(value = "SELECT groupid, group_name, description, first_name, last_name from study_groups sg NATURAL JOIN users u WHERE u.uid=sg.creatorid AND sg.creatorid = :creatorId", nativeQuery = true)
+  @Query(value = "SELECT groupID, group_name, description, first_name, last_name from study_groups sg NATURAL JOIN users u WHERE u.uid=sg.creatorID AND sg.creatorID = :creatorId", nativeQuery = true)
   StudyGroupAndInstructor getById(int creatorId);
 
-  @Query(value = "SELECT groupid, group_name, description, first_name, last_name from study_groups sg NATURAL JOIN users u WHERE u.uid=sg.creatorid AND sg.group_name LIKE :query;", nativeQuery = true)
+  @Query(value = "SELECT sg.groupID, sg.group_name, sg.description, u.first_name, u.last_name FROM study_groups sg " +
+          "INNER JOIN users u ON sg.creatorID=u.uID " +
+          "WHERE (sg.group_name LIKE :query OR sg.description LIKE :query);", nativeQuery = true)
   List<StudyGroupAndInstructor> search(String query);
 }
