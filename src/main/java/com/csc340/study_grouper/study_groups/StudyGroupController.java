cb75489e5865d51eb74110e688f9161dc7c4dab5
Bennett <bascott1002@gmail.com>
2024-11-17 22:04:03 -0500

    Updated student views

--- src/main/java/com/csc340/study_grouper/study_groups/StudyGroupController.java
+++ src/main/java/com/csc340/study_grouper/study_groups/StudyGroupController.java
@@ -1,7 +1,9 @@
 package com.csc340.study_grouper.study_groups;
 
 import com.csc340.study_grouper.messages.MessagesService;
+import com.csc340.study_grouper.users.UserService;
 import com.csc340.study_grouper.users.instructor.InstructorService;
+import com.csc340.study_grouper.users.student.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,16 +22,33 @@ public class StudyGroupController {
     @Autowired
     InstructorService instructorService;
 
+    @Autowired
+    UserService userService;
 
-    @GetMapping("/{gID}")
-    public String studyGroupPage(Model model, @PathVariable int gID){
+    @Autowired
+    StudyGroupService groupService;
+
+    @GetMapping("/instructor/{gID}")
+    public String instructorStudyGroupPage(Model model, @PathVariable int gID){
         model.addAttribute("group", studyGroupService.getStudyGroupByID(gID));
         model.addAttribute("messages", messageService.userMessageJoin(gID));
         return "provider-view/provider-group-view";
     }
 
+    @GetMapping("/student/{gID}")
+    public String studentStudyGroupPage(Model model, @PathVariable int gID){
+      model.addAttribute("student", userService.getUserByID(2));
+      model.addAttribute("courses", groupService.findByCreatorId(2).orElse(null));
+      model.addAttribute("group", studyGroupService.getStudyGroupByID(gID));
+      model.addAttribute("messages", messageService.userMessageJoin(gID));
+
+      return "customer-view/customer-group-view";
+    }
+
     @GetMapping("/search")
     public String searchPage(Model model) {
+        model.addAttribute("student", userService.getUserByID(2));
+        model.addAttribute("courses", groupService.findByCreatorId(2).orElse(null));
         model.addAttribute("studyGroups", studyGroupService.searchStudyGroups(""));
 
         return "customer-view/find-group";
@@ -37,6 +56,8 @@ public class StudyGroupController {
 
     @PostMapping("/search")
     public String searchStudyGroups(String query, Model model) {
+      model.addAttribute("student", userService.getUserByID(2));
+      model.addAttribute("courses", groupService.findByCreatorId(2).orElse(null));
       model.addAttribute("studyGroups", studyGroupService.searchStudyGroups(query));
 
       return "customer-view/find-group";
@@ -44,6 +65,8 @@ public class StudyGroupController {
 
     @GetMapping("/group-description/{groupID}")
     public String groupDescription(@PathVariable int groupID, Model model) {
+      model.addAttribute("student", userService.getUserByID(2));
+      model.addAttribute("courses", groupService.findByCreatorId(2).orElse(null));
       model.addAttribute("groupName", studyGroupService.getStudyGroupAndInstructorById(groupID).getGroupName());
       model.addAttribute("description", studyGroupService.getStudyGroupAndInstructorById(groupID).getDescription());
 
--- src/main/java/com/csc340/study_grouper/users/student/StudentController.java
+++ src/main/java/com/csc340/study_grouper/users/student/StudentController.java
@@ -1,16 +1,30 @@
 package com.csc340.study_grouper.users.customer;
 
+import com.csc340.study_grouper.study_groups.StudyGroupService;
+import com.csc340.study_grouper.users.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @GetMapping("/account")
-    public String account(){
-        return "customer-view/account";
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    StudyGroupService groupService;
+
+    @GetMapping("/account/{pID}")
+    public String account(Model model, @PathVariable int pID){
+      model.addAttribute("student", userService.getUserByID(pID));
+      model.addAttribute("courses", groupService.findByCreatorId(pID).orElse(null));
+
+      return "customer-view/account";
     }
 
     @GetMapping("/biology")
@@ -38,18 +52,27 @@ public class StudentController {
         return "customer-view/software-engineering";
     }
 
-    @GetMapping("/group-description")
-    public String groupDescription(){
-        return "customer-view/group-description";
+    @GetMapping("/group-description/{pID}")
+    public String groupDescription(Model model, @PathVariable int pID){
+      model.addAttribute("student", userService.getUserByID(pID));
+      model.addAttribute("courses", groupService.findByCreatorId(pID).orElse(null));
+
+      return "customer-view/group-description";
     }
 
-    @GetMapping("/home")
-    public String home(){
-        return "customer-view/customer-home";
+    @GetMapping("/home/{pID}")
+    public String home(Model model, @PathVariable int pID) {
+      model.addAttribute("student", userService.getUserByID(pID));
+      model.addAttribute("courses", groupService.findByCreatorId(pID).orElse(null));
+
+      return "customer-view/customer-home";
     }
 
-    @GetMapping("/write-review")
-    public String review(){
-        return "customer-view/chat-settings";
+    @GetMapping("/write-review/{pID}")
+    public String review(Model model, @PathVariable int pID) {
+      model.addAttribute("student", userService.getUserByID(pID));
+      model.addAttribute("courses", groupService.findByCreatorId(pID).orElse(null));
+
+      return "customer-view/chat-settings";
     }
 }
