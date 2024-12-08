948c5a514e020811f82fb9f72164b4ea55bdd6ac
Bennett <bascott1002@gmail.com>
2024-12-08 14:54:17 -0500

    Chuck Norris has been undeployed for bug testing

--- src/main/java/com/csc340/study_grouper/GeneralController.java
+++ src/main/java/com/csc340/study_grouper/GeneralController.java
@@ -3,6 +3,9 @@ package com.csc340.study_grouper;
 import com.csc340.study_grouper.study_groups.StudyGroupService;
 import com.csc340.study_grouper.users.User;
 import com.csc340.study_grouper.users.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
@@ -10,6 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Adam Cichoski, Bennet Scott, Logan Keiper
@@ -81,4 +89,27 @@ public class GeneralController {
         int uID = user.getuID();
         return "redirect:/"+accountType+"/account/"+uID;
     }
+
+    /*@GetMapping("/error")
+    public String error(Model model) {
+        try {
+            String url = "https://api.chucknorris.io/jokes/random?category=history";
+            RestTemplate restTemplate = new RestTemplate();
+            ObjectMapper mapper = new ObjectMapper();
+
+            String jsonListResponse = restTemplate.getForObject(url, String.class);
+            JsonNode root = mapper.readTree(jsonListResponse);
+
+            model.addAttribute("quote", root.get("value").asText());
+
+            return "error";
+        } catch (JsonProcessingException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE,
+                    null, ex);
+
+            model.addAttribute("quote", "Chuck Norris broke the joke.");
+
+            return "error";
+        }
+    }*/
 }
