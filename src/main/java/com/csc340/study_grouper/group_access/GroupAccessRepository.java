800dc6deec15dc037840b2282cb7e004aba19bb8
Adam Cichoski <cjadam003@gmail.com>
2024-12-08 19:43:15 -0500

    Updated the query in groupaccess to appropriately reflect the column names in mySQL

--- src/main/java/com/csc340/study_grouper/group_access/GroupAccessRepository.java
+++ src/main/java/com/csc340/study_grouper/group_access/GroupAccessRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface GroupAccessRepository extends JpaRepository<GroupAccess, Integer> {
 
-    @Query(value = "SELECT * FROM groupaccess WHERE userID = :userID", nativeQuery = true)
+    @Query(value = "SELECT * FROM groupaccess WHERE uID = :userID", nativeQuery = true)
     List<GroupAccess> findByUserID (int userID);
 
     @Query(value = "SELECT * FROM groupaccess WHERE groupID = :groupID", nativeQuery = true)
